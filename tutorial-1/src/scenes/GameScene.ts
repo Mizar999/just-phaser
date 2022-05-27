e8fab98340c6cb32eb2fda922f99794d49d8b5bb
Mario <mizar2502@aol.com>
2022-05-27 13:10:49 +0200

    Move paddle

--- tutorial-1/src/scenes/GameScene.ts
+++ tutorial-1/src/scenes/GameScene.ts
@@ -1,6 +1,10 @@
-import { Physics, Scene } from "phaser";
+import { GameObjects, Physics, Scene, Types } from "phaser";
 
 export class GameScene extends Scene {
+    cursors: Types.Input.Keyboard.CursorKeys;
+    paddleLeft: GameObjects.Rectangle;
+    paddleSpeed: number;
+
     preload(): void {
 
     }
@@ -8,17 +12,31 @@ export class GameScene extends Scene {
     create(): void {
         const ball = this.add.circle(400, 250, 10, 0xffffff, 1);
         this.physics.add.existing(ball);
+
         const bodyBall = ball.body as Physics.Arcade.Body;
         bodyBall.setCollideWorldBounds(true, 1, 1);
         bodyBall.setBounce(1, 1);
-        ball.body.velocity.x = -175;
-        ball.body.velocity.y = 175;
+        ball.body.velocity.x = -200;
+        ball.body.velocity.y = 0;
 
-        const paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1);
-        this.physics.add.existing(paddleLeft, true);
-        const bodyPaddleLeft = paddleLeft.body as Physics.Arcade.StaticBody;
-        bodyPaddleLeft.setMass(100);
+        this.paddleSpeed = 0.5;
+        this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1);
+        this.physics.add.existing(this.paddleLeft, true);
 
-        this.physics.add.collider(paddleLeft, ball);
+        this.physics.add.collider(this.paddleLeft, ball);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update(time: number, delta: number): void {
+        const bodyPaddleLeft = this.paddleLeft.body as Physics.Arcade.StaticBody;
+        if (this.cursors.up.isDown) {
+            this.paddleLeft.y -= this.paddleSpeed * delta;
+            bodyPaddleLeft.updateFromGameObject();
+        }
+        else if (this.cursors.down.isDown) {
+            this.paddleLeft.y += this.paddleSpeed * delta;
+            bodyPaddleLeft.updateFromGameObject();
+        }
     }
 }
