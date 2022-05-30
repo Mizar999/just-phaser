89441739595f72e6b02091684ed49a04965d207d
Mario <mizar2502@aol.com>
2022-05-30 13:13:16 +0200

    Add score and random angle

--- tutorial-1/src/scenes/GameScene.ts
+++ tutorial-1/src/scenes/GameScene.ts
@@ -2,14 +2,22 @@ import { GameObjects, Math, Physics, Scene, Types } from "phaser";
 
 export class GameScene extends Scene {
     cursors: Types.Input.Keyboard.CursorKeys;
+    leftScoreLabel: GameObjects.Text;
+    rightScoreLabel: GameObjects.Text;
     ball: GameObjects.Arc;
     paddleLeft: GameObjects.Rectangle;
     paddleRight: GameObjects.Rectangle;
     paddleSpeed: number;
+    leftScore: number;
+    rightScore: number;
+    aiSpeed: number;
     paddleRightVelocity: Math.Vector2;
 
     init(): void {
         this.paddleSpeed = 0.5;
+        this.aiSpeed = 3;
+        this.leftScore = 0;
+        this.rightScore = 0;
         this.paddleRightVelocity = new Math.Vector2(0, 0);
     }
 
@@ -18,14 +26,15 @@ export class GameScene extends Scene {
     }
 
     create(): void {
-        this.ball = this.add.circle(400, 250, 10, 0xffffff, 1);
+        this.physics.world.setBounds(-100, 0, this.scale.width + 200, this.scale.height);
+
+        this.ball = this.add.circle(this.scale.width / 2, this.scale.height / 2, 10, 0xffffff, 1);
         this.physics.add.existing(this.ball);
 
         const bodyBall = this.ball.body as Physics.Arcade.Body;
         bodyBall.setCollideWorldBounds(true, 1, 1);
         bodyBall.setBounce(1, 1);
-        this.ball.body.velocity.x = Phaser.Math.Between(-200, 200);
-        this.ball.body.velocity.y = Phaser.Math.Between(-200, 200);
+        this.resetBall();
 
         this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1);
         this.physics.add.existing(this.paddleLeft, true);
@@ -35,6 +44,16 @@ export class GameScene extends Scene {
 
         this.physics.add.collider(this.paddleLeft, this.ball);
         this.physics.add.collider(this.paddleRight, this.ball);
+
+        const scoreStyle = {
+            fontSize: "48px"
+        };
+
+        this.leftScoreLabel = this.add.text(300, 125, "0", scoreStyle);
+        this.leftScoreLabel.setOrigin(0.5, 0.5);
+
+        this.rightScoreLabel = this.add.text(500, 375, "0", scoreStyle);
+        this.rightScoreLabel.setOrigin(0.5, 0.5);
 
         this.cursors = this.input.keyboard.createCursorKeys();
     }
@@ -54,12 +73,47 @@ export class GameScene extends Scene {
         const offset = this.paddleRight.height / 2 - 2;
 
         if (this.ball.y < this.paddleRight.y - offset) {
-            this.paddleRight.y -= 3;
+            this.paddleRight.y -= this.aiSpeed;
             body.updateFromGameObject();
         }
         else if (this.ball.y > this.paddleRight.y + offset) {
-            this.paddleRight.y += 3;
+            this.paddleRight.y += this.aiSpeed;
             body.updateFromGameObject();
         }
+
+        if (this.ball.x < -this.ball.radius) {
+            // scored on the left side
+            this.changeScore(0, 1);
+            this.resetBall();
+        }
+        else if (this.ball.x > this.scale.width + this.ball.radius) {
+            // scored on the right side
+            this.changeScore(1, 0);
+            this.resetBall();
+        }
+    }
+
+    private changeScore(leftScore: number, rightScore: number): void {
+        this.leftScore += leftScore;
+        this.leftScoreLabel.text = this.leftScore.toString();
+        this.rightScore += rightScore;
+        this.rightScoreLabel.text = this.rightScore.toString();
+    }
+
+    private resetBall(): void {
+        let angle: number;
+        switch (Math.Between(0, 2)) {
+            case 0:
+                angle = Math.Between(10, 50);
+            case 1:
+                angle = Math.Between(130, 230);
+            default:
+                angle = Math.Between(310, 350);
+        }
+
+        const vector = this.physics.velocityFromAngle(angle, 200);
+        this.ball.body.velocity.x = vector.x;
+        this.ball.body.velocity.y = vector.y;
+        this.ball.setPosition(this.scale.width / 2, this.scale.height / 2)
     }
 }
